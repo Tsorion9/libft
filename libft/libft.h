079377f86c607809888a1469a26b62131bb962eb
Tsorion9 <tsorion_9@mail.ru>
2022-09-25 10:29:31 +0300

    ft_isalnum to coding standard

--- libft/libft.h
+++ libft/libft.h
@@ -38,7 +38,7 @@ char				*ft_strstr(const char *haystack, const char *needle);
 int					ft_strcmp(const char *s1, const char *s2);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
 int					ft_isdigit(int c);
-int					ft_isalnum(int c);
+int                 ftIsalnum(int c);
 int					ft_isalpha(int c);
 int					ft_toupper(int c);
 int					ft_isascii(int c);
